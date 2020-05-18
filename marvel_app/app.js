5d42e0e785bbe900cf524bfd2f0073b5cb68537d
Xavier Johnson <xavierjohnson@xaviers-air.home>
2020-05-17 19:13:10 -0700

    got the carousel to some what work and the api is in

--- marvel_app/app.js
+++ marvel_app/app.js
@@ -1,14 +1,45 @@
 $(() => {
-  console.log($);
+  let currentImgIndex = 0
+  const numOfImages = $('.carousel-images').children().length - 1;
+
+  $('.next').on('click', () => {
+    $('carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    if(currentImgIndex < numOfImages) {
+      currentImgIndex ++;
+    } else {
+      currentImgIndex = 0;
+    }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  $('.previous').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+    if(currentImgIndex > 0) {
+      currentImgIndex --;
+    } else {
+      currentImgIndex = numOfImages
+    }
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  $('.button').on('click', event => {
+    event.preventDefault()
+
+
+
+  })
+
+//   let character = $(event.target).
+// console.log(character);
 const link = "https://rickandmortyapi.com/api/character/"
 
 $.ajax({
-  Url: link,
-  type: 'GET',
-}). then(reports => {
+  url: link,
+  type: 'GET'
+}).then(reports => {
   console.log(reports);
 })
 .catch(err=>{
-  console.log(eer);
+  console.log(err);
 })
 })
